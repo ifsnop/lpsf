e2c8011d026f0569f147f322aeebcb823979d42e
Diego Torres <diego.torres@gmail.com>
2013-05-15 20:28:17 +0200

    typo

--- functions_log.inc.php
+++ functions_log.inc.php
@@ -1,4 +1,22 @@
 <?php
+/*
+    Light PHP scrapper framework. Several php utilities to scrap sites.
+    Copyright (C) 2013 Diego Torres <diego dot torres at gmail dot com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 function lpsf_log($where, $str) {
     global $lpsf;
@@ -10,3 +28,4 @@ function lpsf_log($where, $str) {
     
     return;
 }
+
