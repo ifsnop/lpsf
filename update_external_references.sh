e102a8ce3bb7024ae573e988cf08260dd6bad6bd
Diego Torres <diego.torres@gmail.com>
2013-07-16 21:36:53 +0200

    updated external references, added twitter-api-php and nicejson-php

--- update_external_references.sh
+++ update_external_references.sh
@@ -18,3 +18,15 @@ git clone git://github.com/rapid2k1/mysqldump-php.git mysqldump-php
 cd mysqldump-php
 git pull
 cd ..
+
+git clone https://github.com/Keeguon/nicejson-php.git nicejson-php
+cd nicejson-php
+git pull
+cd ..
+
+git clone https://github.com/J7mbo/twitter-api-php.git twitter-api-php
+cd twitter-api-php
+git pull
+cd ..
+
+
